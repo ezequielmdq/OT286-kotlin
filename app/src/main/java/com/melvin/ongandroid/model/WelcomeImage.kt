b0939bf79e771dc4e23536ef85bb7eac6f04436b
Raynel Perez <raynelmayorca@hotmail.com>
2022-08-19 15:01:43 -0300

    Agregado de mensaje de bienvenida y lista de imagenes

---
+++ app/src/main/java/com/melvin/ongandroid/model/WelcomeImage.kt
@@ -0,0 +1,6 @@
+package com.melvin.ongandroid.model
+
+data class WelcomeImage(val image: String,
+                        val tittle: String,
+                        val description: String
+                       )
