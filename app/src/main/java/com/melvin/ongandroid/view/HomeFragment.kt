0f27688f497d446a6a28911bdb6d857438023da1
Raynel Perez <raynelmayorca@hotmail.com>
2022-08-22 20:43:03 -0300

    algunos cambios visuales en la cardView

--- app/src/main/java/com/melvin/ongandroid/view/HomeFragment.kt
+++ app/src/main/java/com/melvin/ongandroid/view/HomeFragment.kt
@@ -13,7 +13,6 @@ import com.melvin.ongandroid.model.WelcomeImage
 class HomeFragment : Fragment() {
     private var binding: FragmentHomeBinding? = null
 
-
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -31,17 +30,19 @@ class HomeFragment : Fragment() {
      */
     private fun configTestimonios() {
         val list = listOf(
-            Testimonio("Jose", "Lo mejor", " bla bla", "https://loremflickr.com/320/240/dog"),
+            Testimonio("Jose", "Lo mejor", " Todos en el Privilege Club se esmeraron y nos mimaron. Hemos recibido de ellos el mejor de los tratos y la mejor voluntad para solucionar unos pequeños problemas personales. Gustavo, Carlos, José y Morgan nos consintieron, junto a Lorena y el resto de los chicos del bar.", "https://loremflickr.com/320/240/dog"),
             Testimonio("Gaston", "Lo Peorsito", "  bla bla", "https://loremflickr.com/320/240"),
             Testimonio("Mica", "Majomeno", "Como estan bla bla", "https://loremflickr.com/g/320/240/paris"),
             Testimonio("Nose", "Ahi", "Como estan bla bla", "https://loremflickr.com/320/240/brazil,rio"),
         )
 
-        binding?.cardTestimonios?.apply {
-            bindTestimonio(cardTestimonio1, list[0])
-            bindTestimonio(cardTestimonio2, list[1])
-            bindTestimonio(cardTestimonio3, list[2])
-            bindTestimonio(cardTestimonio4, list[3])
+        binding?.let { it ->
+            it.cardTestimonios.apply {
+                bindTestimonio(cardTestimonio1, list[0])
+                bindTestimonio(cardTestimonio2, list[1])
+                bindTestimonio(cardTestimonio3, list[2])
+                bindTestimonio(cardTestimonio4, list[3])
+            }
         }
 
     }
@@ -57,16 +58,18 @@ class HomeFragment : Fragment() {
      */
     private fun configWelcomeList(){
         val list = listOf(
-            WelcomeImage("https://loremflickr.com/320/240", "Hola", "Como estan bla bla"),
+            WelcomeImage("https://loremflickr.com/320/240", "Hola", "Todos en el Privilege Club se esmeraron y nos mimaron. Hemos recibido de ellos el mejor de los tratos y la mejor voluntad para solucionar unos pequeños problemas personales. Gustavo, Carlos, José y Morgan nos consintieron, junto a Lorena y el resto de los chicos del bar."),
             WelcomeImage("https://loremflickr.com/320/240/dog", "Hola", "Como estan bla bla"),
             WelcomeImage("https://loremflickr.com/g/320/240/paris", "Hola", "Como estan bla bla"),
             WelcomeImage("https://loremflickr.com/320/240/brazil,rio", "Hola", "Como estan bla bla"),
             WelcomeImage("https://loremflickr.com/g/320/240/paris,girl/all", "Hola", "Como estan bla bla")
         )
 
+        //instancio el adapter
         val adapter = ListAdapter()
-        //lista temporal
+        //seteo la lista al adapter
         adapter.list.addAll(list)
+        //seteo el adapter al recyclerView "Binenvenidos"
         binding?.let {
             it.rv.adapter = adapter
         }
