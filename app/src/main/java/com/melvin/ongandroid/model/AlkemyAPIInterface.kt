45d5518615f5484d9fa7eb09b859ec75d9acc210
Ezequiel <ezereinoso@gmail.com>
2022-08-24 23:48:54 -0300

    Agregado función de llamada a api news

--- app/src/main/java/com/melvin/ongandroid/model/AlkemyAPIInterface.kt
+++ app/src/main/java/com/melvin/ongandroid/model/AlkemyAPIInterface.kt
@@ -1,5 +1,6 @@
 package com.melvin.ongandroid.model
 
+import com.melvin.ongandroid.model.data.NovedadData
 import com.melvin.ongandroid.model.data.WelcomeData
 import retrofit2.Response
 import retrofit2.http.GET
@@ -10,5 +11,8 @@ interface AlkemyAPIInterface {
 
     @GET("slides")
     suspend fun getData() : Response<WelcomeData>
+
+    @GET("news")
+    suspend fun getDataNovedad() : Response<NovedadData>
 
 }
