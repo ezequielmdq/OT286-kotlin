b33a4054fe03b2baa22a6e6e3e4cca1a74853c16
JeantyJr <pierrejeantyjunior@gmail.com>
2022-08-29 19:52:43 -0300

    sin cambio

--- app/src/main/java/com/melvin/ongandroid/model/repository/Network/retrofit/AlkemyAPIInterface.kt
+++ app/src/main/java/com/melvin/ongandroid/model/repository/Network/retrofit/AlkemyAPIInterface.kt
@@ -1,5 +1,6 @@
 package com.melvin.ongandroid.model.repository.Network.retrofit
 
+import com.melvin.ongandroid.model.Novedad
 import com.melvin.ongandroid.model.data.NovedadData
 import com.melvin.ongandroid.model.data.TestimonioData
 import com.melvin.ongandroid.model.data.WelcomeData
