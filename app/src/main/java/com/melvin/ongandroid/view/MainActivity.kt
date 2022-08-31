beea719983b3cb62d007de9d3968cdffdc61521a
Lautaro Casanueva <casanuevalautaro@gmail.com>
2022-08-31 20:39:48 -0300

    diseño de la vista de contacto y navegacion

--- app/src/main/java/com/melvin/ongandroid/view/MainActivity.kt
+++ app/src/main/java/com/melvin/ongandroid/view/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -13,7 +14,7 @@ import com.melvin.ongandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var navController: NavController
+    private lateinit var navController : NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,17 +22,21 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        supportActionBar?.setDisplayShowTitleEnabled(false)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.NavHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+      /*  supportActionBar?.setDisplayShowTitleEnabled(false)
         navController = findNavController(R.id.NavHostFragment)
         appBarConfiguration = AppBarConfiguration(setOf(R.id.firstFragment, R.id.firstFragment2))
         setupActionBarWithNavController(navController,appBarConfiguration)
+        */
 
-        binding.bottomNavigation.setupWithNavController(findNavController(R.id.NavHostFragment))
+        binding.bottomNavigation.setupWithNavController(navController)
 
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.NavHostFragment)
+        //val navController = findNavController(R.id.NavHostFragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
