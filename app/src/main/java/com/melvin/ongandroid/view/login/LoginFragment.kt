7d53ba288b1f87c3f9e728f5a1ac77ce547b3b84
Raynel Perez <raynelperezmayorca@gmail.com>
2022-09-07 23:21:05 -0300

    Creacion del layout Login

---
+++ app/src/main/java/com/melvin/ongandroid/view/login/LoginFragment.kt
@@ -0,0 +1,22 @@
+package com.melvin.ongandroid.view.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.melvin.ongandroid.R
+import com.melvin.ongandroid.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
