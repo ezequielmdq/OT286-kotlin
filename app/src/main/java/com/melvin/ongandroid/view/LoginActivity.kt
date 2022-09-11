0f0fd2e088811c85b3f234fccf120b3a0efeae61
Ezequiel <ezereinoso@gmail.com>
2022-09-10 22:18:18 -0300

    Login Facebook

--- app/src/main/java/com/melvin/ongandroid/view/LoginActivity.kt
+++ app/src/main/java/com/melvin/ongandroid/view/LoginActivity.kt
@@ -3,10 +3,15 @@ package com.melvin.ongandroid.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.melvin.ongandroid.R
+import com.melvin.ongandroid.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/com/melvin/ongandroid/view/login/LoginFragment.kt
+++ app/src/main/java/com/melvin/ongandroid/view/login/LoginFragment.kt
@@ -1,25 +1,34 @@
 package com.melvin.ongandroid.view.login
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
-import com.melvin.ongandroid.R
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import com.melvin.ongandroid.application.ONGApplication.Companion.prefs
 import com.melvin.ongandroid.databinding.FragmentLoginBinding
-
 import com.melvin.ongandroid.model.LogIn
 import com.melvin.ongandroid.model.repository.Network.implement.LogInDataRepository
+import com.melvin.ongandroid.view.LoginActivity
+import com.melvin.ongandroid.view.MainActivity
 import com.melvin.ongandroid.viewmodel.LogInViewModel
 import com.melvin.ongandroid.viewmodel.LogInViewModelFactory
+import java.util.*
+
 
 class LoginFragment : Fragment() {
 
      lateinit var binding: FragmentLoginBinding
+     private val callbackManager = CallbackManager.Factory.create()
 
     private val viewModel : LogInViewModel by activityViewModels(
         factoryProducer = {
@@ -34,10 +43,38 @@ class LoginFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentLoginBinding.inflate(inflater, container, false)
 
+
         binding.btLogin.setOnClickListener {
             viewModel.logIn(LogIn(binding.tiEmail.text.toString(), binding.tiContrasenia.text.toString()))
         }
 
+        /** estos metodos maneja el resultado del icinio de sesion. Si es satisfactorio se ejecutara lo
+         * que esta en onSucces, sy se cancela lo que esta en on Cancel y si hay algun error
+         * lo que esta en on error
+         */
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+            object : FacebookCallback<LoginResult> {
+                override fun onCancel() {
+                    LoginManager.getInstance().logOut()
+
+                }
+
+                override fun onError(error: FacebookException) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onSuccess(result: LoginResult) {
+                    val intent = Intent(context, MainActivity::class.java)
+                    startActivity(intent)
+                }
+            })
+
+        /** listener boton de facebook para inicio de sesion*/
+
+        binding.btnFacebook?.setOnClickListener{
+            LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile"));
+        }
 
         configObservables()
         return binding.root
@@ -50,6 +87,15 @@ class LoginFragment : Fragment() {
             prefs.saveToken(viewModel.token.value.toString())
 
         })
+    }
+
+    /** el método onActivityResult, llama a callbackManager.onActivityResult para pasar
+    los resultados del inicio de sesión al objeto LoginManager mediante callbackManager.*/
+
+    @Override
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
 
